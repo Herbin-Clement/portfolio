1ad32b44e95e60d611c5a55169de3fcbc86c902f
herbin <clement.herbin.etu@univ-lille.fr>
2021-10-11 17:43:01 +0200

    update tailwind config and add folder components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      black0: '#121212',
+      black05: '#202020',
+      black1: '#303030',
+      concrete: '#95a5a6',
+      cloud: '#ecf0f1',
+      silver: '#bdc3c7',
+      sunFlower: '#f1c40f',
+      orange: '#f39c12',
+      wet: '#34495e',
+      midnight: '#2c3e50',
+      amethist: '#9b59b6',
+
+    },
+    extend: {
+      spacing: {
+        '1/10': '10%',
+        '2/10': '20%',
+        '3/10': '30%',
+        '4/10': '40%',
+        '5/10': '50%',
+        '6/10': '60%',
+        '7/10': '70%',
+        '8/10': '80%',
+        '9/10': '90%',
+        '10/10': '100%',
+        '1/12': '8%',
+        '11/12': '92%',
+        '5p': '5%',
+        '8p': '8%',
+        '15p': '15%',
+      }
+    },
   },
   variants: {
     extend: {},
