9afbb3408a25eaf7ac63544ac17fe530be3da7d3
herbin <clement.herbin.etu@univ-lille.fr>
2021-10-12 20:33:17 +0200

    update ProjectCard

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -3,12 +3,12 @@ import ProjectCard from "./ProjectCard";
 
 const Project = () => {
     return (
-        <div className="flex flex-col items-center w-100p h-80 p-10 bg-white">
+        <div className="flex flex-col items-center w-100p h-60v p-10 bg-white">
             <Title text="Projects." color="text-black80"/>
-            <div className="flex justify-evenly items-center w-80p h-70p border">
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
+            <div className="flex justify-evenly items-center w-80p h-70p">
+                <ProjectCard title="Fluid Simulation" language="C++" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+                <ProjectCard title="Port Folio" language="React JS - Tailwind" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+                <ProjectCard title="Board Game" language="Java" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
             </div>
         </div>
     );
--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,7 +1,9 @@
-const ProjectCard = ({}) => {
+const ProjectCard = ({title, language, description}) => {
     return(
-        <div>
-
+        <div className="flex flex-col items-center w-30p h-100p p-5 bg-black21 rounded-sm">
+            <div className="mb-6 text-orange text-3xl font-bold">{title}</div>
+            <div className="mb-4 text-white text-xl font-bold">{language}</div>
+            <div className="text-black80 text-l font-bold text-center">{description}</div>
         </div>
     );
 };
